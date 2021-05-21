4a627cc6c42f7ba0656ab16c4c06bb3a808d74a8
Maryury Canales <Maryu_canales@hotmail.lcom>
2021-05-20 23:24:07 -0600

    + Home landing section

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { LandingRoutingModule } from './landing.routing';
+import { LandingComponent } from './landing.component';
+import { FuseNavigationModule } from '@fuse/components/navigation';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    LandingComponent
   ],
   imports: [
     CommonModule,
     LandingRoutingModule,
+    FuseNavigationModule,
   ]
 })
 export class LandingModule { }
