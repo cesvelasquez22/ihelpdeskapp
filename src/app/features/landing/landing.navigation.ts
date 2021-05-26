7c1f916bcc19d786812beb208d9f6e9e4b903a51
Mxcixs <ricardomaciaszapata8@gmail.com>
2021-05-25 21:57:31 -0600

    Commit de prueba

---
+++ src/app/features/landing/landing.navigation.ts
@@ -0,0 +1,28 @@
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const landingNavigation: FuseNavigationItem[] = [
+    {
+        id: 'homea',
+        title: 'Inicio',
+        type: 'basic',
+        icon: 'mat_outline:home',
+        link: 'home',
+    },
+    {
+        id: 'about',
+        title: 'Sobre nosotrosA',
+        type: 'basic',
+        icon: 'mat_outline:group',
+        link: 'about',
+    },
+    {
+        id: 'pricing',
+        title: 'Planes',
+        type: 'basic',
+        icon: 'mat_outline:attach_money',
+        link: 'pricing',
+    },
+    {
+        type: 'divider',
+    },
+];
