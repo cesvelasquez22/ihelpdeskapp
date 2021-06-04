4d0a032bf022e975eb17ee72f37a541d3a07c4f8
Maryurycanales <maryu_canales@hotmail.com>
2021-06-04 09:02:59 -0600

    +Listado roles

--- src/app/features/users/components/roles/roles.component.ts
+++ src/app/features/users/components/roles/roles.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Role } from '../../models/role.interface';
+import { RolesService } from '../../services/roles.service';
 
 @Component({
   selector: 'app-roles',
   templateUrl: './roles.component.html',
   styleUrls: ['./roles.component.scss']
 })
-export class RolesComponent implements OnInit {
+export class RolesComponent implements OnInit, OnDestroy {
+  roles: Role[] = [];
 
-  constructor() { }
+  private unsubscribe$ = new Subject<void>();
+  constructor(
+    private rolesService: RolesService
+  ) { }
 
   ngOnInit(): void {
+    this.getRoles();
   }
 
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  getRoles() {
+    this.rolesService.getAllRoles().pipe(takeUntil(this.unsubscribe$)).subscribe(roles => {
+      if (roles && roles.length > 0) {
+        this.roles = roles;
+      }
+    })
+  }
 }
