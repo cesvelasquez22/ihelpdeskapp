fd7efe322dc2336838691b4e3ee069a7aad888cd
paola.medina2708 <paola.sabillon278@gmail.com>
2021-06-04 18:32:38 -0600

    Listado de departamentos

--- src/app/features/maintenances/components/departments/departments.component.ts
+++ src/app/features/maintenances/components/departments/departments.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Department } from '../../models/department.interface';
+import { DepartmentsService } from '../../services/departments.service';
 
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
   styleUrls: ['./departments.component.scss']
 })
-export class DepartmentsComponent implements OnInit {
+export class DepartmentsComponent implements OnInit, OnDestroy {
+  departments: Department[] = [];
+  
+  private unsubscribe$ = new Subject<void>();
 
-  constructor() { }
+  constructor(
+    private departmentsService: DepartmentsService
+  ) { }
 
   ngOnInit(): void {
+    this.getCustomers();
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  getCustomers() {
+    this.departmentsService.getAllDepartments().pipe(takeUntil(this.unsubscribe$)).subscribe(departments => {
+      if (departments && departments.length > 0) {
+        this.departments = departments;
+      }
+    })
   }
 
 }
