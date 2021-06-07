61b46b23fb8466952ff8c799c4a3174bde07fcda
Mxcixs <ricardomaciaszapata8@gmail.com>
2021-06-07 15:49:03 -0600

    + User detail form

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -8,6 +8,7 @@ import { RoleDetailComponent } from './components/role-detail/role-detail.compon
 import { UserDetailComponent } from './components/user-detail/user-detail.component';
 import { UsersService } from './services/users.service';
 import { RolesService } from './services/roles.service';
+<<<<<<< Updated upstream
 
 
 @NgModule({
@@ -22,5 +23,32 @@ import { RolesService } from './services/roles.service';
     UsersRoutingModule
   ],
   providers: [UsersService, RolesService],
+=======
+import { SharedModule } from 'app/shared/shared.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+    declarations: [
+        UsersComponent,
+        RolesComponent,
+        RoleDetailComponent,
+        UserDetailComponent,
+    ],
+    imports: [
+        CommonModule,
+        UsersRoutingModule,
+        SharedModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+    ],
+    providers: [UsersService, RolesService],
+>>>>>>> Stashed changes
 })
-export class UsersModule { }
+export class UsersModule {}
