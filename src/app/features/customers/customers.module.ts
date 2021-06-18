d2d157ba49cbe601311e2c8f8f30435a98edde5b
Mxcixs <ricardomaciaszapata8@gmail.com>
2021-06-17 21:08:42 -0600

    + Customer form and updateCustomer method

--- src/app/features/customers/customers.module.ts
+++ src/app/features/customers/customers.module.ts
@@ -7,9 +7,34 @@ import { CustomersComponent } from './components/customers/customers.component';
 import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
 import { CustomersService } from './services/customers.service';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Material
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+
 @NgModule({
     declarations: [CustomersComponent, CustomerDetailComponent],
-    imports: [CommonModule, CustomersRoutingModule, SharedModule],
+    imports: [
+        CommonModule,
+        CustomersRoutingModule,
+        SharedModule,
+        ReactiveFormsModule,
+
+        // Material
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSelectModule,
+        MatDialogModule,
+    ],
     providers: [CustomersService],
 })
 export class CustomersModule {}
