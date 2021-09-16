8d79cfa426c5888dfde1461fb7807e401c0b65d1
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2021-09-15 19:20:44 -0600

    + Search tickets

--- src/app/features/tickets/components/tickets/tickets.component.ts
+++ src/app/features/tickets/components/tickets/tickets.component.ts
@@ -1,22 +1,21 @@
 import {ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import { Location } from '@angular/common';
-import { FormControl } from '@angular/forms';
-import { MatDrawer } from '@angular/material/sidenav';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FuseMediaWatcherService } from '@fuse/services/media-watcher';
-import { TitleHeader } from 'app/core/models/title-header.model';
-import { of, Subject } from 'rxjs';
+import {Location} from '@angular/common';
+import {FormControl} from '@angular/forms';
+import {MatDrawer} from '@angular/material/sidenav';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FuseMediaWatcherService} from '@fuse/services/media-watcher';
+import {TitleHeader} from 'app/core/models/title-header.model';
+import {combineLatest, ReplaySubject, Subject} from 'rxjs';
 import {
-    catchError,
     debounceTime,
     distinctUntilChanged,
     map,
     takeUntil,
-    tap,
+    tap
 } from 'rxjs/operators';
-import { Ticket } from '../../models/ticket.interface';
-import { TicketsService } from '../../services/tickets.service';
-import { User } from 'app/features/users/models/user.interface';
+import {Ticket} from '../../models/ticket.interface';
+import {TicketsService} from '../../services/tickets.service';
+import {User} from 'app/features/users/models/user.interface';
 
 @Component({
     selector: 'app-tickets',
@@ -31,8 +30,8 @@ export class TicketsComponent implements OnInit, OnDestroy {
     };
     loading = false;
 
-    @ViewChild('matDrawer', { static: true }) matDrawer: MatDrawer;
-    tickets: Ticket[] = [];
+    @ViewChild('matDrawer', {static: true}) matDrawer: MatDrawer;
+    private tickets: Ticket[] = [];
     adminUsers: User[] = [];
     ticketsCount = 0;
     searching = false;
@@ -40,7 +39,9 @@ export class TicketsComponent implements OnInit, OnDestroy {
     // DRAWER
     drawerMode: 'side' | 'over';
 
-    searchInputControl: FormControl = new FormControl();
+    // RELATED TO FILTER
+    filteredInfo$ = new ReplaySubject<Ticket[]>(1);
+    filterControl = new FormControl();
     selectedTicket: Ticket;
 
     // UNSUBSCRIBE ALL
@@ -53,33 +54,19 @@ export class TicketsComponent implements OnInit, OnDestroy {
         private location: Location,
         private _changeDetectorRef: ChangeDetectorRef,
         private _fuseMediaWatcherService: FuseMediaWatcherService
-    ) {}
+    ) {
+    }
 
     ngOnInit(): void {
-        this.getTickets();
-        this.getAdminUsers();
-        this.searchInputControl.valueChanges
+        this.getTicketsInfo();
+        this.filterControl.valueChanges
             .pipe(
                 takeUntil(this.unsubscribe$),
                 map((query: string) => (query ? query.trim() : '')),
                 debounceTime(150),
                 distinctUntilChanged(),
-                tap(() => (this.searching = true)),
-                // switchMap((query : string) => {
-                //     return this.customersService.searchCustomers(query).pipe(
-                //       tap(customers => {
-                //         this.customersCount = customers.length;
-                //       })
-                //     );
-                // }),
-                tap(() => (this.searching = false)),
-                catchError(() => {
-                    this.searching = false;
-                    this.ticketsCount = 0;
-                    return of([]);
-                })
             )
-            .subscribe((changes) => console.log(changes));
+            .subscribe((search) => (this.applyFilter(search)));
 
         // Subscribe to MatDrawer opened change
         this.matDrawer.openedChange.subscribe((opened) => {
@@ -95,7 +82,7 @@ export class TicketsComponent implements OnInit, OnDestroy {
         // Subscribe to media changes
         this._fuseMediaWatcherService.onMediaChange$
             .pipe(takeUntil(this.unsubscribe$))
-            .subscribe(({ matchingAliases }) => {
+            .subscribe(({matchingAliases}) => {
                 // Set the drawerMode if the given breakpoint is active
                 if (matchingAliases.includes('lg')) {
                     this.drawerMode = 'side';
@@ -108,30 +95,32 @@ export class TicketsComponent implements OnInit, OnDestroy {
             });
     }
 
-    ngOnDestroy() {
+    ngOnDestroy(): void {
         this.unsubscribe$.next();
         this.unsubscribe$.complete();
     }
 
-    getTickets() {
+    getTicketsInfo(): void {
         this.loading = true;
-        this.ticketsService
-            .getAllTickets()
-            .pipe(takeUntil(this.unsubscribe$))
-            .subscribe(
-                (tickets) => {
-                    this.ticketsCount = tickets.length;
-                    if (tickets && tickets.length > 0) this.tickets = tickets;
-                    this.loading = false;
-                },
-                (err) => (this.loading = false)
-            );
-    }
+        const ticketsInfo$ = combineLatest(
+            this.ticketsService.getAllTickets(),
+            this.ticketsService.getAdminUsers(),
+        );
 
-    getAdminUsers() {
-        this.ticketsService
-            .getAdminUsers()
-            .subscribe((users) => (this.adminUsers = users));
+        ticketsInfo$
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe((data) => {
+                if (data[0] && data[0].length > 0) {
+                    this.filteredInfo$.next(data[0]);
+                    this.tickets = data[0];
+                    this.ticketsCount = data[0].length;
+                }
+
+                if (data[1] && data[1].length > 0) {
+                    this.adminUsers = data[1];
+                }
+                this.loading = false;
+            }, (err) => (this.loading = false));
     }
 
     /**
@@ -143,7 +132,7 @@ export class TicketsComponent implements OnInit, OnDestroy {
         this.matDrawer.close();
     }
 
-    openPreview(ticket: Ticket) {
+    openPreview(ticket: Ticket): void {
         // Get the current activated route
         this.ticketsService.ticket = ticket;
         this.selectedTicket = ticket;
@@ -152,18 +141,32 @@ export class TicketsComponent implements OnInit, OnDestroy {
             while (route.firstChild) {
                 route = route.firstChild;
             }
-            this.router.navigate(['preview'], { relativeTo: route });
+            this.router.navigate(['preview'], {relativeTo: route});
         }
 
         // Mark for check
         this._changeDetectorRef.markForCheck();
     }
 
-    assignTicketTo(user: User) {
+    private applyFilter(search: string): void {
+        if (!search) {
+            this.filteredInfo$.next(this.tickets);
+        } else {
+            search = search.toLowerCase();
+        }
+
+        this.filteredInfo$.next(
+            this.tickets.filter((ticket) => ticket.subject.toLowerCase().indexOf(search) > -1 || ticket.uid.toLowerCase().indexOf(search) > -1)
+        );
+
+        this.filteredInfo$.pipe(takeUntil(this.unsubscribe$), tap((tickets) => this.ticketsCount = tickets.length)).subscribe();
+    }
+
+    assignTicketTo(user: User): void {
         console.log(user);
     }
 
-    currentState(key: string) {
+    currentState(key: string): string {
         return this.ticketsService.currentState(key).label;
     }
 }
